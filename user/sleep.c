d76d1f47615e7a4eaa6056c34b1c5dc92f968657
shaoguixin <2404987607@qq.com>
2022-03-29 16:22:05 +0800

    finish sleep

---
+++ user/sleep.c
@@ -0,0 +1,17 @@
+//
+// Created by 邵桂鑫 on 2022/3/29.
+//
+
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[]) {
+    if (argc <= 1) {
+        fprintf(2, "usage : sleep seconds\n");
+        exit(0);
+    }
+    sleep(atoi(argv[1]));
+    exit(0);
+}
