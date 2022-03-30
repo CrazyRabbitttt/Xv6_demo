3f688ff5b72b54f6572a53cf094b402da52f1f97
shaoguixin <2404987607@qq.com>
2022-03-30 17:18:20 +0800

    finished primes, tested ok

--- user/primes.c
+++ user/primes.c
@@ -10,55 +10,71 @@
 
 #define FIRST_PRIME 2
 
-int
-generate_natual() {
+int generate();
+
+int filter(int in_fd, int prime);
+
+int main()
+{
+    int prime;
+    int in = generate();
+    while (read(in, &prime, sizeof (int))) {
+        printf("prime %d\n", prime);
+        in = filter(in, prime);
+    }
+    exit(0);
+}
+
+int generate()
+{
     int fd[2];
+
     pipe(fd);
-    int pid = fork();
-    if (pid == 0) {
-        close(fd[1]);
-        return fd[0];
-    }else {
+    if (!fork()) {      //parent
         for (int i = FIRST_PRIME; i <= MAXNUM; i++) {
             write(fd[1], &i, sizeof (int));
         }
         close(fd[1]);
         exit(0);
     }
+    //father will never got here
+    //here is the child to do
+    close(fd[1]);
+    return fd[0];
+
 }
 
 int
-prime_handle(int in_fd, int prime) {
-    //to guolv prime number
+filter(int in_fd, int prime)
+{
+    int value;
     int fd[2];
-    int num;            //store the number you read
-
-    //主要是关闭 写的端口，防止子进程阻塞？
-    int pid = fork();
-    if (pid == 0) {
-        close(in_fd);
-        close(fd[1]);
-        return fd[0];
-    }else {
-        while (read(in_fd, &num, sizeof (int))) {
-            if (num % prime) {
-                write(fd[1], &num, sizeof (int));
+    pipe(fd);
+    if (!fork()) {
+        while (read(in_fd, &value, sizeof (int))) {
+            if (value % prime) {
+                write(fd[1], &value, sizeof (int));
             }
         }
         close(in_fd);
         close(fd[1]);
+
+
         exit(0);
     }
+
+    //here also never have father process
+    close(in_fd);
+    close(fd[1]);
+    return fd[0];
+
 }
 
-int main()
-{
-    int prime;
-    int in = generate_natual();     //the number of read descripter
-    while (read(in, &prime, sizeof (int))) {
-        printf("prime %d\n", prime);
-        in = prime_handle(in, prime);
-    }
-}
+
+
+
+
+
+
 
 
