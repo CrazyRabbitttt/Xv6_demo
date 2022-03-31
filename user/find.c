c20e99a767f274e1819e30495203a72dd2fa8357
CrazyRabbitttt <2404987607@qq.com>
2022-03-31 18:53:11 +0600

    The docker

--- user/find.c
+++ user/find.c
@@ -1,7 +1,3 @@
-//
-// Created by SGX on 2022/3/30.
-//
-
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
@@ -11,77 +7,77 @@
 char*
 fmtname(char *path)
 {
-    static char buf[DIRSIZ+1];
-    char *p;
+	char *p;
 
-    // Find first character after last slash.
-    //means open file by fmtname
-    for(p=path+strlen(path); p >= path && *p != '/'; p--)
-        ;
-    p++;
+	// Find first character after last slash.
+	for(p=path+strlen(path); p >= path && *p != '/'; p--)
+		;
+	p++;
 
-    return buf;
+	return p;
 }
 
-void find(char * path, char *target) {
-    char buf[502], *p;
-    int fd;
-    struct dirent de;
-    struct stat st;
+void
+find(char *path, char *targetname) 
+{
+	char buf[512], *p;
+	int fd;
+	struct dirent de;
+	struct stat st;
 
-    if (!strcmp(fmtname(path), target)) {
-        printf("%s\n", path);           //if fmt is equal, print the old path
-        return ;
-    }
+	if (!strcmp(fmtname(path), targetname)) {
+		printf("%s\n", path);
+	}
 
-    if ((fd = open(path, O_RDONLY)) < 0) {
-        fprintf(2, "find can open %s, the fd is %d", path, fd);
-        exit(1);
-    }
+	if ((fd = open(path, O_RDONLY)) < 0) {
+		fprintf(2, "find: cannot open [%s], fd=%d\n", path, fd);
+		return;
+	}
 
-    if (fstat(fd, &st) < 0) {
-        fprintf(2, "find: cant stas %s\n", path);
-        close(fd);
-        return;
-    }
+	if (fstat(fd, &st) < 0) {
+		fprintf(2, "find: cannot stat %s\n", path);
+		close(fd);
+		return;
+	}
 
+	if (st.type != T_DIR) {
+		close(fd);
+		return;
+	}
 
-    if (st.type == T_FILE) {
-        close(fd);
-        return ;        //not equal
-    }
+	// st.type == T_DIR
+	
+	if (strlen(path) + 1 + DIRSIZ + 1 > sizeof buf) {
+		printf("find: path too long\n");
+		close(fd);
+		return;
+	}
+	strcpy(buf, path);
+	p = buf+strlen(buf);
+	*p++ = '/';
+	while (read(fd, &de, sizeof(de)) == sizeof(de)) {
+		if (de.inum == 0)
+			continue;
+		memmove(p, de.name, DIRSIZ);
+		p[DIRSIZ] = 0;
+		
+		if (!strcmp(de.name, ".") || !strcmp(de.name, ".."))
+			continue;
 
-    if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
-        printf("ls: path too long\n");
-        close(fd);
-        return ;
-    }
-    strcpy(buf, path);
-    p = buf+strlen(buf);
-    *p++ = '/';
-
-    while (read(fd, &de, sizeof (de)) == sizeof (de)) {
-        if (de.inum == 0)
-            continue;
-        memmove(p, de.name, DIRSIZ);
-        p[DIRSIZ] = 0;          //add path: old + filename
-
-        if (!strcmp(de.name, ".") || !strcmp(de.name, ".."))
-            continue;
-        //now the file's path blow the dir already add to 'path'
-        find(buf, target);
-    }
-    close(fd);
-
+		find(buf, targetname);
+	}
+	close(fd);
 }
 
 int
 main(int argc, char *argv[])
 {
-    if (argc < 3) {
-        fprintf(2,"Usage : find <path> <filename>\n");
-        exit(1);
-    }
-    find(argv[1], argv[2]);
-    exit(0);
+	if(argc < 3){
+		fprintf(2, "usage: find path filename\n");
+		exit(1);
+	}
+
+	find(argv[1], argv[2]);
+
+	exit(0);
 }
