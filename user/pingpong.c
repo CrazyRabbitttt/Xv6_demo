d7a42e8bfc094d6488869d5f0047fcb2805aa340
shaoguixin <2404987607@qq.com>
2022-06-30 12:05:45 +0800

    finish pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -1,36 +1,41 @@
 //
-// Created by SGX on 2022/3/30.
+// Created by SGX on 2022/6/30.
 //
 
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
 
-#define BUFSIZE 16
+#define SENDSIZE 16
 
-int main()
-{
-    int pid;
+int main(int argc, char** argv) {
+
+    //Parent send byte to child, child receive and send back
     int fd[2];
-    pipe(fd);
-    char buf[BUFSIZE];
-    pid = fork();
-    if (pid == 0) {     /*child*/
-        read(fd[0], buf, 4);
-        close(fd[0]);
-        printf("%d: received %s\n", getpid(), buf);
-        write(fd[1], "pong", 4);
-        close(fd[1]);
-        exit(0);
-    }else {
-        write(fd[1], "ping", 4);
-        close(fd[1]);
-        wait(0);        //wait for child end...
-
-        read(fd[0], buf, 4);
-        printf("%d: received %s\n", getpid(), buf);
-        close(fd[0]);
-        exit(0);
+    char sendbuf[SENDSIZE];
+    char recvbuf[SENDSIZE];
+    pid_t pid;
+    sendbuf = "Ping";
+    if (pipe(fd) < 0) {
+        printf("create pipe error, quit\n");
+        exit(1);
     }
+    if ((pid = fork()) < 0) {
+        printf("create process error, quit\n");
+        exit(1);
+    } else if (pid > 0) {       //Parent
+        write(fd[1], sendbuf, SENDSIZE);        //send message to pipe[1], and
+
+        sleep(1);
+        int n = read(fd[0], sendbuf, SENDSIZE);
+        printf("%d: received pong\n", getpid());
+    } else {                    //Child
+        int n = read(fd[0], recvbuf, SENDSIZE);
+        if (n) {
+            printf("%d: received ping\n", getpid());
+            write(fd[1], sendbuf, SENDSIZE);            //send back
+        }
+    }
+
 }
 
